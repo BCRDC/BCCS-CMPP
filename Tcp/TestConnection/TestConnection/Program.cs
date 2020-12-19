5c1ecd16e451bf00a103c934f5524a422f5b3f08
unknown <v-jiwag@hotmail.com>
2020-12-19 10:23:18 +0000

    connection for tcp

---
+++ Tcp/TestConnection/TestConnection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestConnection
+{
+    class Program
+    {
+        const int PORT_NO = 4123;
+        const string SERVER_IP = "mcvp-vm";
+
+        static void Main(string[] args)
+        {
+            //---listen at the specified IP and port no.---
+            C.Connect(SERVER_IP, "WHO", PORT_NO);
+        }
+    }
+}
