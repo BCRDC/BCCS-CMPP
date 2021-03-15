9f49a62331be30a33d8f2a0e25ee692c28ad5d69
Jin Wang <jin.wang@bcrdteam.onmicrosoft.com>
2021-03-15 03:49:29 +0000

    internal http post

---
+++ Clients/http.internal.post.ps1
@@ -0,0 +1,30 @@
+
+
+$Account = "jiwagTest"
+$Sig = "jiwagsig"
+
+$Header = @{
+        "SmsProvider" = "967584f9-af0b-424c-9b24-2c1799ef99d2"
+        "Account" = $Account
+    }
+
+$MessageBody = @{
+    CandidateSig = $Sig
+    Text = "hello test"
+}
+
+$Body = @{
+    PhoneNumber =   @("18321676517")
+    Extend = "01"
+    MessageBody = $MessageBody 
+    CorrelationId = (New-Guid).toString()
+}
+ 
+$Parameters = @{
+    Method = "POST"
+    Uri =  "https://localhost:5001/internalservices/sms/txts"
+    Body = ($Body | ConvertTo-Json) 
+    ContentType = "application/json"
+    Headers = $Header
+}
+Invoke-RestMethod @Parameters
